19235d0224239661d2a147f8fd7bafdb3eee2215
WilliamDPrince <55257106+WilliamDPrince@users.noreply.github.com>
2020-10-20 16:24:48 -0500

    Add files via upload

--- mysite/Glizzymail/admin.py
+++ mysite/Glizzymail/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Tutorial
+from tinymce.widgets import TinyMCE
+from django.db import models
 # Register your models here.
 
-admin.site.register(Tutorial)
+class TutorialAdmin(admin.ModelAdmin):
+	
+	fieldsets = [
+		("Title/date", {"fields": ["tutorial_title", "tutorial_published"]}),
+		("Content", {"fields":["tutorial_content"]})
+		]
+	formfield_overrides= {
+	models.TextField: {'widget': TinyMCE()}
+	}
+admin.site.register(Tutorial, TutorialAdmin)
