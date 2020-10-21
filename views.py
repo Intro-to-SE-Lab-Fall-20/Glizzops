b9412e29c0d8362c8e3e878f922c2f7bd0e07b72
WilliamDPrince <55257106+WilliamDPrince@users.noreply.github.com>
2020-10-20 20:12:18 -0500

    Add files via upload

---
+++ views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Tutorial
+
+def homepage(request):
+	return render(request=request,
+				template_name ="Glizzymail/home.html",
+				context={"tutorials": Tutorial.objects.all})
