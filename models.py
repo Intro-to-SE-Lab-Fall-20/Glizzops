c7b34e610878105f97c26ccbde57202420b19184
WilliamDPrince <55257106+WilliamDPrince@users.noreply.github.com>
2020-10-23 12:42:43 -0500

    Add files via upload
    This is the full project with all the functionalities I have made up to this date. It can send emails, but I have an error causing it to display html in the email itself that I just can't seem to figure out how to fix.
    
    The first 10 files are to be put into a mysite folder in the base directory.
    
    The rest must be in a folder titled Glizzymail. The Glizzymail _pychache_, migrations, and .html files must all be in their own folders.
    Name the .html file folder as templates.
    
    This version requires at least python 3.8 and Django installed in the base directory of the first folder for the website.

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
+
 
  #Create your models here.
 class Tutorial(models.Model):
@@ -10,3 +12,37 @@ class Tutorial(models.Model):
 	def __str__(self):
 		return self.tutorial_title
 
+class TimestampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Inbox(TimestampMixin):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner_inboxes')
+    guess = models.ForeignKey(User, on_delete=models.CASCADE, related_name='guess_inboxes')
+
+    def __str__(self):
+        return self.owner.username + ' - ' + self.guess.username
+
+    class Meta:
+        verbose_name_plural = 'inboxes'
+
+
+class Message(TimestampMixin):
+    inbox = models.ForeignKey(Inbox, on_delete=models.CASCADE, related_name='messages')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages')
+    content = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.content[:25]
+
+    def save(self, *args, **kwargs):
+        self.inbox.save()
+        super(Message, self).save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['created_at']
--- views.py
+++ views.py
@@ -1,8 +1,92 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Tutorial
+from django.shortcuts import render, redirect
+from .models import Tutorial, TimestampMixin, Inbox, Message
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout, authenticate, login
+from django.contrib import messages
+from .forms import NewUserForm, ComposeForm
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.conf import settings
+
+
+# Create your views here.
+def homepage(request):
+    return render(request = request,
+                  template_name='Glizzymail/home.html',
+                  context = {"tutorials":Tutorial.objects.all})
+
+def register(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New account created: {username}")
+            login(request, user)
+            return redirect("Glizzymail:homepage")
+
+        else:
+            for msg in form.error_messages:
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
+
+            return render(request = request,
+                          template_name = "Glizzymail/register.html",
+                          context={"form":form})
+
+    form = NewUserForm
+    return render(request = request,
+                  template_name = "Glizzymail/register.html",
+                  context={"form":form})
+
+def logout_request(request):
+	logout(request)
+	messages.info(request, "Logged out successfully!")
+	return redirect("Glizzymail:homepage")
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('/')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request = request,
+                    template_name = "Glizzymail/login.html",
+                    context={"form":form})
+
+def compose(request):
+    if request.method == 'GET':
+        form = ComposeForm()
+    else:
+        form = ComposeForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            email_address = form.cleaned_data['email_address']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, email_address, ['glizzymailtest@gmail.com']) 
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect ("Glizzymail:homepage")
+
+    form = ComposeForm()
+    return render(request, "Glizzymail/compose.html", {'form':form})
 
 def homepage(request):
-	return render(request=request,
-				template_name ="Glizzymail/home.html",
-				context={"tutorials": Tutorial.objects.all})
+    return render(request = request,
+                  template_name='Glizzymail/home.html',
+                  context = {"tutorials":Tutorial.objects.all})
+
+def Inbox(request):
+    return render(request = request,
+                  template_name='Glizzymail/inbox.html',
+                  context = {"messages":Message.objects.all})
